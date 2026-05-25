9471647eedf5d99341961689a48c0f2cb3879f41
Danielle D. <dani.dixonpro@gmail.com>
2026-05-24 22:08:48 -0400

    Fixed predict method args

--- apputil.py
+++ apputil.py
@@ -10,15 +10,16 @@ def GroupEstimate(object):
     #1. Group df by columns in X, abstracted
     #2. for.ea. calculate mean or med. of y, depending on estimate arg
         if GroupEstimate(estimate='mean'):
-            return coffee_reviews.groupby(X.columns).agg('rating', 'mean')
+            return df_coffee.groupby(X.columns).agg(['mean'])
         else:
-            return coffee_reviews.groupby(X.columns).agg(['median'])
+            return df_coffee.groupby(X.columns).agg(['median'])
 
-    def predict(self, X):
+    def predict(self, X_):
     #Takes an array of obv. corresponding to columns in X_,
     # determines appropriate groups, and returns est. for y 
     # return Nan for any missing combinations of data
     # print missing no. of groups 
+
         return None
     
 """Initialized df for testing 
@@ -27,20 +28,20 @@ DF based on possible existing pokemon type combinations and likelihood of captur
 
 coffee_reviews = {
     "loc_country": ["Guatemala", "Japan", "Fiji", "Fiji", "Japan", "Japan", "Fiji", "Guatemala"],
-    "roast": ["Dark", "Medium", "Medium", "Dark", "Dark", "Light", "Medium", "Dark"],
+    "roast": ["Dark", "Medium", "Medium", "Dark", "Dark", "Medium", "Medium", "Dark"],
     "rating": [17.22, 84.34, 39.01, 52.01, 71.89, 25.57, 96.18]
 }
 
-df = pd.DataFrame(coffee_reviews)
+df_coffee = pd.DataFrame(coffee_reviews)
 
-X = coffee_reviews[["loc_country", "roast"]]
-y = coffee_reviews["rating"]
+X = df_coffee[["loc_country", "roast"]]
+y = df_coffee["rating"]
 
 gm = GroupEstimate(estimate='mean')
 gm.fit(X,y)
 
 X_ = [["Japan", "Medium"],
-      ["Guatemala", "Dark"], #There should be no dark guatemala
-      ["Fiji", "Light"]] 
+      ["Guatemala", "Dark"], 
+      ["Fiji", "Light"]] # no light Fiji
 
 gm.predict(X_)
